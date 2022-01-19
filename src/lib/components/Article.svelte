0d164f2a45ab044536307200336b208f948ba763
xiayulu <harolddfinchh@gmail.com>
2022-01-19 21:44:21 +0800

    feature: using markdown

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -1,14 +1,19 @@
 <script>
+  import SvelteMarkdown from 'svelte-markdown';
   export let article;
 </script>
 
-<div >
+<div>
   <h2>{article.name}</h2>
-  <p class="text-center py-3"><span><strong>作者：</strong> {article.author}</span> | <span><strong>时间：</strong>{article.created_at}</span></p>
+  <p class="text-center py-3">
+    <span><strong>作者：</strong> {article.author}</span> | <span><strong>时间：</strong>{article.created_at}</span>
+  </p>
   <div class="mb-5">
     <img class="cover" src={article.cover} alt="封面" />
   </div>
-  <p>{article.content}</p>
+  <div>
+    <SvelteMarkdown source={article.content} />
+  </div>
 </div>
 
 <style>
