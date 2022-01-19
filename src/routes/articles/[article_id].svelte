8818c8d99a0352feaabe5497b90fb8572198164c
xiayulu <harolddfinchh@gmail.com>
2022-01-19 21:44:03 +0800

    feature:using backend api

--- src/routes/articles/[article_id].svelte
+++ src/routes/articles/[article_id].svelte
@@ -1,14 +1,20 @@
+<script context="module">
+  export const load = async ({ fetch, params, session }) => {
+    const article_id = params.article_id;
+    const res = await fetch(`http://127.0.0.1:8000/articles/${article_id}/`);
+    const article = await res.json();
+    return {
+      props: {
+        article
+      }
+    };
+  };
+</script>
+
 <script>
   import Article from '$lib/components/Article.svelte';
 
-  export let article = {
-    id: 1,
-    name: 'undefined',
-    author: 'Whu Lambda',
-    created_at: '2022-01-18',
-    cover: '/logo.png',
-    content: `undefined`
-  };
+  export let article;
 </script>
 
 <Article {article} />
