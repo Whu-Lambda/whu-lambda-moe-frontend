7a1659fa86e6d176a44b29c4e16a0d25f8a8ec06
xiayulu <harolddfinchh@gmail.com>
2022-01-19 21:35:43 +0800

    feature: using backend api

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,25 @@
+<script context="module">
+  export const load = async ({ fetch, session }) => {
+    const articles_res = await fetch('http://127.0.0.1:8000/articles/');
+    const articles = await articles_res.json();
+    return {
+      props: {
+        articles
+      }
+    };
+  };
+</script>
+
 <script>
   import Start from '$lib/components/Start.svelte';
   import ActivityStart from '$lib/components/ActivityStart.svelte';
   import ArticleStart from '$lib/components/ArticleStart.svelte';
+
+  export let articles;
 </script>
 
 <Start />
 
 <ActivityStart />
 
-<ArticleStart />
+<ArticleStart {articles} />
