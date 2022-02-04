9b22ad7c458544bca0be911da90277ee958d6b09
TlMegalovania <tlmegalovania@qq.com>
2022-02-05 00:24:06 +0800

    fix: dep **** npm

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ RUN npm run build
 
 FROM node:16.13-alpine
 WORKDIR /app
-COPY --from=builder /app/out .
-ENTRYPOINT [ "node", "out" ]
+COPY --from=builder /app/out/ ./
+ENTRYPOINT [ "node", "." ]
 EXPOSE 3000
--- src/lib/utils/grpc.js
+++ src/lib/utils/grpc.js
@@ -2,7 +2,7 @@ import grpc from '@grpc/grpc-js';
 import protoLoader from '@grpc/proto-loader';
 
 const url = 'https://whu-lambda.moe/api/v1';
-const protoPromise = protoLoader.load('../protos/whu_lambda_web.proto').then((def) => grpc.loadPackageDefinition(def).whu.lambda.moe.dto);
+const protoPromise = protoLoader.load('static/whu_lambda_web.proto').then((def) => grpc.loadPackageDefinition(def).whu.lambda.moe.dto);
 const anonymousPromise = protoPromise.then((proto) => new proto.Anonymous(url, grpc.credentials.createSsl()));
 const authedPromise = protoPromise.then((proto) => new proto.Authenticated(url, grpc.credentials.createSsl()));
 
