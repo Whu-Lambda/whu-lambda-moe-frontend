c61f22cf59f50f168a77581b918556faa6a97f06
xiayulu <harolddfinchh@gmail.com>
2022-02-07 19:47:29 +0800

    feat: using api

--- src/routes/articles/index.svelte
+++ src/routes/articles/index.svelte
@@ -1,15 +1,15 @@
 <script>
   import ArticleCard from '$lib/components/ArticleCard.svelte';
-  // import { anonymousPromise } from '$lib/utils/grpc';
+  import { anonymousPromise } from '$lib/utils/grpc';
   let articles;
-  // const load = async () => {
-  //   const client = await anonymousPromise;
-  //   const stream = client.getArticles({});
-  //   stream.on('data', (arti) => {
-  //     articles = articles.push(arti);
-  //   });
-  // };
-  // load();
+  const load = async () => {
+    const client = await anonymousPromise;
+    const stream = client.getArticles({});
+    stream.on('data', (arti) => {
+      articles = articles.push(arti);
+    });
+  };
+  load();
 </script>
 
 {#if articles}
